dabc5bb4e51d19baa0959d56f4914c97785f3340
Lich <longzy2013@gmail.com>
2019-12-03 00:04:26 +0800

    Integer Replacement

---
+++ Integer Replacement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        count = 0
+        while n > 3:
+            print(n)
+            count += 1
+            if n & 1 == 0:
+                n >>= 1
+            elif n & 2 == 0:
+                n -= 1
+            else: # n & 3 == 3:
+                n += 1
+
+        return count+n-1
+
+if __name__ == '__main__':
+    print(Solution().integerReplacement(8))
