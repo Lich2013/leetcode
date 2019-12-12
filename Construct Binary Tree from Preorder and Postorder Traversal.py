20f219a5bd699bf2867e599d5243ee7f7e191a1f
Lich <longzy2013@gmail.com>
2019-12-13 00:14:30 +0800

    Construct Binary Tree from Preorder and Postorder Traversal

---
+++ Construct Binary Tree from Preorder and Postorder Traversal.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
+        if len(pre) == 0:
+            return None
+        root = TreeNode(pre[0])
+        if len(pre) <= 1:
+        	return root
+        index = post.index(pre[1])+1
+        root.left = self.constructFromPrePost(pre[1:index+1], post[:index+1])
+        root.right = self.constructFromPrePost(pre[index+1:], post[index:-1])
+        return root
+
+
+if __name__ == '__main__':
+    print(Solution().constructFromPrePost([1,2,4,5,3,6,7], [4,5,2,6,7,3,1]))
